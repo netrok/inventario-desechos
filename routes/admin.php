9ce187cceaee3fd0f87cd495849f8cca1f67384a
netrok <ernesto_antonio@aol.com>
2026-01-13 18:59:46 -0600

    feat(admin): modulo usuarios con permisos + UI por can()

--- routes/admin.php
+++ routes/admin.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\Admin\UserController;
 
 Route::prefix('admin')
     ->name('admin.')
-    ->middleware(['auth', 'role:Admin'])
+    ->middleware(['web', 'auth', 'permission:usuarios.gestionar'])
     ->group(function () {
         Route::resource('users', UserController::class)->except(['show']);
     });
